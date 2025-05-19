407e429dd6942c576df7fcdb3bacd945aa18e001
devanfedhi <devanfedhi1@gmail.com>
2025-05-19 16:40:41 +1000

    Updated resume and skills

--- webapp-frontend/src/sections/Experience/Experience.jsx
+++ webapp-frontend/src/sections/Experience/Experience.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import styles from './ExperienceStyles.module.css'
-import ExperienceBlock from '../ExperienceBlock/ExperienceBlock'
+import React from "react";
+import styles from "./ExperienceStyles.module.css";
+import ExperienceBlock from "../ExperienceBlock/ExperienceBlock";
 
 function Experience() {
-  const pattern = ['X','|', '-', '-', '|', 'X'];
+  const pattern = ["X", "|", "-", "-", "|", "X"];
   const experiences = [
-    { title: "DevOps Engineer", company: "Coles Group", duration: "Jun 2024 - Dec 2024"},
-    { title: "Deep Learning Engineer", company: "Monash Deep Neuron", duration: "Aug 2023 - Jun 2024"},
-    { title: "Commercial Coworker", company: "IKEA", duration: "Mar 2023 - Jun 2024"},
-    { title: "Software Engineer", company: "MCAV", duration: "Aug 2022 - Apr 2023"},
-    { title: "Customer Service Coworker", company: "Hungry Jacks", duration: "Nov 2021 - Feb 2023"},
+    {
+      title: "Software Engineer",
+      company: "Coles Group",
+      duration: "Feb 2025 - Current",
+    },
+    {
+      title: "DevOps Engineer",
+      company: "Coles Group",
+      duration: "Jun 2024 - Dec 2024",
+    },
+    {
+      title: "Deep Learning Engineer",
+      company: "Monash Deep Neuron",
+      duration: "Aug 2023 - Jun 2024",
+    },
+    {
+      title: "Commercial Coworker",
+      company: "IKEA",
+      duration: "Mar 2023 - Jun 2024",
+    },
+    {
+      title: "Software Engineer",
+      company: "MCAV",
+      duration: "Aug 2022 - Apr 2023",
+    },
+    {
+      title: "Customer Service Coworker",
+      company: "Hungry Jacks",
+      duration: "Nov 2021 - Feb 2023",
+    },
   ];
 
   for (let i = 0; i < experiences.length; i++) {
@@ -17,48 +42,49 @@ function Experience() {
   }
   let experienceIndex = 0;
   let lineIndex = 0;
-  const totalBlocks = experiences.length*3 ;
+  const totalBlocks = experiences.length * 3;
 
   return (
     <section className={styles.container}>
       <div className={styles.cascadingTable}>
         {Array.from({ length: totalBlocks }).map((_, index) => {
           const patternIndex = index % pattern.length;
-          if (pattern[patternIndex] === 'X' && experienceIndex < experiences.length) {
+          if (
+            pattern[patternIndex] === "X" &&
+            experienceIndex < experiences.length
+          ) {
             return (
-              <ExperienceBlock className={styles.item} data={experiences[experienceIndex++]} />
+              <ExperienceBlock
+                className={styles.item}
+                data={experiences[experienceIndex++]}
+              />
             );
-          } else if (pattern[patternIndex] === '|'){
-            if (lineIndex < experiences.length - 1 ) {
+          } else if (pattern[patternIndex] === "|") {
+            if (lineIndex < experiences.length - 1) {
               return (
-
                 lineIndex++,
-                <div className={styles.shape}>
-                  <div className={styles.circle}></div>
-                  <div className={styles.line}></div>
-                </div>
-
-                
+                (
+                  <div className={styles.shape}>
+                    <div className={styles.circle}></div>
+                    <div className={styles.line}></div>
+                  </div>
+                )
               );
             } else {
               return (
-
                 <div className={styles.shape}>
                   <div className={styles.circleLone}></div>
                   <div className={styles.lineLone}></div>
                 </div>
               );
             }
-            
-          }  else {
-            return (
-              <div></div>
-            );
+          } else {
+            return <div></div>;
           }
         })}
       </div>
     </section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
